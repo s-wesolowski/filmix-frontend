a5168acd47c6d464bc462bfb58b0003fbb247cf4
s-wesolowski <szymonstrum@wp.pl>
2022-03-23 21:37:57 +0100

    main

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
